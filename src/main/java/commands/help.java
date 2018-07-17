f9d02e831f5d8973943eed0d775d514a22566683
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2018-07-18 00:54:00 +0200

    Update help.java

--- src/main/java/commands/help.java
+++ src/main/java/commands/help.java
@@ -14,14 +14,15 @@ public class help implements runinterface {
     @Override
     public void action(String[] args, MessageReceivedEvent event) {
 
-        event.getTextChannel().sendMessage(
+       event.getTextChannel().sendMessage(
                 new EmbedBuilder().setColor(Color.BLACK)
                         .setTitle("Code:Neo Command List")
                         .setDescription("__**BOT RELATED**__\n``cn:help``, ``cn:github``, ``cn:servercount``, ``cn:usercount``, ``cn:bugreport``, ``cn:info``\n\n" +
                                         "__**MODERATION**__\n``cn:kick``, ``cn:ban``, ``cn:mute``, ``cn:unmute``\n\n" +
                                         "__**SERVER**__\n``cn:serverinfo``\n\n" +
                                         "__**FUN**__\n``cn:say``\n\n" +
-                                        "__**GAME STATS**__\n``cn:paladins``")
+                                        "__**GAME STATS**__\n``cn:paladins``\n\n" +
+                                        "__**MUSIC**__\n``cn:m <play/skip/now/stop>")
                         .setFooter("Created by " + event.getJDA().getUserById("265849018662387712").getName(), event.getJDA().getUserById("265849018662387712").getAvatarUrl())
                         .build()).queue();
 
