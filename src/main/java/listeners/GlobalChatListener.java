df10fcdc5990c27d1b4f74f48f250345be27e990
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2019-03-05 19:03:08 +0100

    Update GlobalChatListener.java

--- src/main/java/listeners/GlobalChatListener.java
+++ src/main/java/listeners/GlobalChatListener.java
@@ -8,8 +8,8 @@ public class GlobalChatListener extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         if(event.getTextChannel().getId().contains("523561706841047064")) {
-            if(event.getMessage().getEmbeds().get(0).getDescription().startsWith(".about")) {
-                event.getTextChannel().sendMessage("**Code:Neo** is the first Bot which got implemented into Kazuma's Global Chat\n" +
+            if(event.getMessage().getEmbeds().get(0).getDescription().contains(".about")) {
+                event.getJDA().getTextChannelById("523561706841047064").sendMessage("**Code:Neo** is the first Bot which got implemented into Kazuma's Global Chat\n" +
                                                         "The Bot is currently on version 1.0 and still in development.\n" +
                                                         "Get your own Bot registered [here](https://discord.gg/fyJ6PFj)\n").queue();
             }
